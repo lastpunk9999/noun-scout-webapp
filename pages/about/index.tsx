c2dfd8d1c37ed12f40265bdcce14dda7666739ff
ripe <unripeengine@gmail.com>
2022-11-13 21:07:10 -0500

    about page

---
+++ pages/about/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+
+const About: NextPage = () => {
+  
+  return (
+    <div className="container mx-auto max-w-lg">
+        <h1 className="text-2xl font-bold mb-2">About Noun Seek</h1>
+        <p>Noun Seek is a project that allows you to sponsor Nouns and send money to charity.</p>
+        <h2 className="text-xl font-bold mt-5 mb-2">Lorem Ipsum</h2>
+        <p>Curabitur blandit tempus porttitor. Nullam quis risus eget urna mollis ornare vel eu leo. Etiam porta sem malesuada magna mollis euismod. Curabitur blandit tempus porttitor.</p>
+        <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur. Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.</p>
+        
+    </div>
+  );
+};
+
+export default About;
