bb9fd896629d0345db93db6f1bd773a61f305104
ripe <unripeengine@gmail.com>
2022-11-18 14:18:05 -0800

    add config option for nouns auction contract

--- config.ts
+++ config.ts
@@ -1,6 +1,12 @@
 import nounSeekABI from "./abi/nounSeekABI";
+import AuctionHouseABI from "./abi/nounsAuctionHouseABI.json";
 
 export const nounSeekContract = {
   address: process.env.NEXT_PUBLIC_NOUNSEEK_ADDRESS,
   abi: nounSeekABI,
+};
+
+export const nounsAuctionHouseContract = {
+  address: process.env.NEXT_PUBLIC_NOUNS_AUCTION_HOUSE_ADDRESS,
+  abi: AuctionHouseABI,
 };
