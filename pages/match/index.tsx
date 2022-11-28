38996d9e5e46f6252420ad1f30218faced558316
punk9999 <punk9999@protonmail.com>
2022-11-28 11:15:36 -0500

    `/match` route does not need to be connected

--- pages/match/index.tsx
+++ pages/match/index.tsx
@@ -8,21 +8,16 @@ import { useIsMounted } from "../../hooks";
 
 const Match: NextPage = () => {
   const { isConnected, isConnecting } = useAccount();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (isConnecting || !router) return;
-    if (isConnected) return;
-    router.push("/");
-  }, [isConnected, isConnecting, router]);
 
   const matchData = useContractRead({
     address: nounSeekContract.address,
     abi: nounSeekContract.abi,
     functionName: 'donationsForMatchableNoun',
   }).data;
+
   // const isMounted = useIsMounted();
-  if (!isConnected || !matchData) return null;
+  if (!matchData) return null;
+
   return (
     <div>
       <h1 className="text-3xl font-bold mb-2 text-center">Open Matches</h1>
