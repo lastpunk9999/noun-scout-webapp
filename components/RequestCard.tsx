2d9431e95b9578bf96cfcd06cddf96d2c2d89881
ripe <unripeengine@gmail.com>
2022-11-30 16:27:11 -0800

    support background trait, add option for placeholders in add request flow

--- components/RequestCard.tsx
+++ components/RequestCard.tsx
@@ -24,27 +24,21 @@ const getPart = (partType: string, partIndex: number) => {
 };
 
 const RequestCard = (props: RequestCardProps) => {
-  const traitTypeId = props.trait?.traitTypeId || 0;
-  const traitId = props.trait?.traitId || undefined;
+  const traitTypeId =
+    props.trait?.traitTypeId >= 0 ? props.trait.traitTypeId : undefined;
+  const traitId = props.trait?.traitId >= 0 ? props.trait.traitId : undefined;
   const traitTypeNames = traitTypeNamesById(traitTypeId) || undefined;
-  const part = traitId && traitTypeNames && getPart(traitTypeNames[1], traitId);
-  console.log(props.donations, "props.donations");
+  const part =
+    traitTypeId > 0 && traitTypeNames && getPart(traitTypeNames[1], traitId);
+
   return (
     <div className="bg-white p-3 w-full rounded-lg border border-slate-200 pb-4">
-      {/* <div>
-          {props.id > 0 && (
-            <>
-              <p>Noun {props.id} specific</p>
-            </>
-          )}
-        </div> */}
       <div className="flex gap-3 items-center">
         <div className="w-2/4">
           {/* Trait image - use bg color from noun if available */}
           <div
             className="aspect-square rounded-lg w-lg relative"
             style={
-              // part &&
               props.nounSeed
                 ? {
                     backgroundColor: `#${
@@ -52,53 +46,58 @@ const RequestCard = (props: RequestCardProps) => {
                     }`,
                   }
                 : {
-                    backgroundColor: `#${ImageData.bgcolors[0]}`,
+                    backgroundColor: `#${ImageData.bgcolors[1]}`,
                   }
             }
           >
             {part ? (
-              <Image
-                src={part.image ? part.image : "/loading-noun.gif"}
-                layout="fill"
-              />
+              <Image src={part.image} layout="fill" />
             ) : (
               <>
-                <div className="opacity-30 relative z-10 h-100">
-                  <Image src="/loading-noun.gif" layout="fill" />
-                </div>
-                {/* <Skeleton
-                  containerClassName="w-full h-full aspect-square absolute top-0 left-0 z-0"
-                  className="w-full h-full"
-                /> */}
+                {/* exception for background color traits */}
+                {traitId >= 0 ? (
+                  <div
+                    className="opacity-30 relative z-10 h-100 aspect-square"
+                    style={{
+                      backgroundColor: `#${ImageData.bgcolors[traitId]}`,
+                    }}
+                  />
+                ) : (
+                  <div className="opacity-30 relative z-10 h-100 aspect-square">
+                    <Image src="/loading-noun.gif" layout="fill" />
+                  </div>
+                )}
               </>
             )}
           </div>
         </div>
         <div className="w-3/4">
           <p className="text-slate-400 text-xs leading-none capitalize">
-            {traitId ? traitTypeNames[0] : "Trait type"}
+            {traitId >= 0 ? traitTypeNames[0] : "Trait type"}
           </p>
           <h3 className="text-xl font-bold leading-none capitalize">
             {traitId !== undefined ? (
               traitNamesById(traitTypeId, traitId)
             ) : (
-              <Skeleton width={100} height={20} />
+              <>Select a Noun trait</>
             )}
           </h3>
           <hr className="my-1 border-slate-500/25" />
           <p className="text-slate-400 text-xs mb-1">Supporting</p>
           <ul className="flex gap-4">
-            {props.donations ? (
-              props.donations.map((donation, i) => {
-                if (donation.amount && !donation.amount.isZero()) {
-                  return <RequestDonee key={i} donation={donation} />;
-                } else {
-                  return <Skeleton width={100} />;
-                }
-              })
-            ) : (
-              <Skeleton width={100} />
-            )}
+            {props.donations
+              ? props.donations.map((donation, i) => {
+                  if (
+                    !donation.amount?.isZero() ||
+                    donation.amount === undefined
+                  ) {
+                    return <RequestDonee key={i} donation={donation} />;
+                  } else {
+                    // return <Skeleton width={100} />;
+                    return "Charity of your choice";
+                  }
+                })
+              : "Charity of your choice"}
           </ul>
         </div>
       </div>
