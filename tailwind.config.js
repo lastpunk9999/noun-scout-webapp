e0250f9e0a5f5e6d543f85a4e2890720ae4558ab
ripe <109935398+ripe0x@users.noreply.github.com>
2022-12-19 15:37:20 -0800

    add color shades to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,28 +15,33 @@ module.exports = {
       display: ["group-hover"],
       colors: {
         ...defaultTheme.colors,
-        // TODO: add color variants here
         blue: {
-          100: '#3E4B5F',
-          200: '#3E4B5F',
-          300: '#3E4B5F',
-          400: '#3E4B5F',
-          500: '#3E4B5F',
-          600: '#3E4B5F',
-          700: '#3E4B5F',
-          800: '#3E4B5F',
-          900: '#3E4B5F',
+          DEFAULT: '#3E4B5F',
+          '50': '#98A7BC',
+          '100': '#8C9CB4',
+          '200': '#7387A4',
+          '300': '#5E7290',
+          '400': '#4E5E78',
+          '500': '#3E4B5F',
+          '600': '#28303D',
+          '700': '#12151B',
+          '800': '#000000',
+          '900': '#000000'
         },
-        'red': '#EB5D2D',
+        red: {
+          DEFAULT: '#EB5D2D',
+          '50': '#FBDED5',
+          '100': '#F9D0C2',
+          '200': '#F6B39D',
+          '300': '#F29678',
+          '400': '#EF7A52',
+          '500': '#EB5D2D',
+          '600': '#CC4213',
+          '700': '#99320F',
+          '800': '#66210A',
+          '900': '#331005'
+        }
       },
-      // TODO: typography overrides aren't working here
-      // typography: (theme) => ({
-      //   DEFAULT: {
-      //     css: {
-      //       color: theme('colors.gray.700'),
-      //     },
-      //   },
-      // }),
     },
   },
   plugins: [
